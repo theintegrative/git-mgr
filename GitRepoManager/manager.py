f89a59d886e09d2a9bf8a4c4bded1d4d297435bf
theintegrative <theintegrative@gmail.com>
2023-05-30 14:47:55 +0200

    replace manager with rewite

--- GitRepoManager/manager.py
+++ GitRepoManager/manager.py
@@ -1,12 +1,13 @@
 import os
-import re
+import glob
 import json
-import typer
-import git
-import concurrent.futures
+from pprint import pprint
+from prompt_toolkit import prompt
+from git import Repo
 
-CONFIG_PATH = "./git/repoconfig.json"
-directories = [f"./git/{org}/{loc}" for org in ["hub", "lab"] for loc in ["int", "ext"]]
+REPO_CONFIG_FILE = "repoconfig.json"
+REPO_CONFIG_PATH = "/home/theintegrative/git-repo-manager/test/git-repo/"
+SEARCH_PATH = os.path.expanduser('~')
 
 def read_file_contents(file_path):
     with open(file_path, 'r') as f:
@@ -16,165 +17,115 @@ def write_data_to_file(file_path, data):
     with open(file_path, 'w') as f:
         json.dump(data, f, indent=4)
 
-def create_dirs():
-    for directory in directories:
-        os.makedirs(directory, exist_ok=True)
+def create_path(extension):
+    base_path = os.getcwd()
+    extended_path = os.path.join(base_path, extension)
+    os.makedirs(extended_path, exist_ok=True)
+    return extended_path
 
-def generate_repolists():
-    repos = {}
-    for directory in directories:
-        repos[directory] = []
-        for root, dirs, files in os.walk(directory):
-            for file in files:
-                if file == 'config':
-                    path = os.path.join(root, file)
-                    with open(path, 'r') as f:
-                        content = f.read()
-                        match = re.search(r'git@[^@]*\.com:[^:]+/[^/]+', content)
-                        if match:
-                            url = match.group(0)
-                            url = re.sub(r'\s+fetch\s*=.*', '', url)
-                            repos[directory].append(url)
-    write_data_to_file(CONFIG_PATH, repos)
+## REPO CONFIG
+def create_repoconfig(directory, repoconfig=REPO_CONFIG_FILE):
+    directory_links = get_repository_links(directory)
+    write_data_to_file(repoconfig, {"links": directory_links})
+    print(f"Created {repoconfig}")
 
-def clone_repositories():
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        repos = read_file_contents(CONFIG_PATH)
-        for directory, urls in repos.items():
-            for url in urls:
-                executor.submit(clone_repository, directory, url)
+def update_repoconfig(directory, repoconfig=REPO_CONFIG_FILE):
+    directory_links = get_repository_links(directory)
+    repoconfig_links = read_file_contents(repoconfig)["links"]
+    write_data_to_file(repoconfig, {"links": get_unique_links(directory_links, repoconfig_links)})
 
-def clone_repository(directory, url):
-    os.system(f"cd {directory} && git clone --quiet {url}")
+def init_repoconfig(directory, repoconfig=REPO_CONFIG_FILE):
+    if not os.path.exists(repoconfig):
+        create_repoconfig(directory, repoconfig)
+    repoconfig_setup(repoconfig)
 
-def check_repoconfig_file(func):
-    def wrapper(*args, **kwargs):
-        if not os.path.isfile(CONFIG_PATH):
-            typer.echo("The repoconfig.json file does not exist. Please run the 'create' command to generate it.")
-            return
-        repos = read_file_contents(CONFIG_PATH)
-        if not repos:
-            typer.echo("The repoconfig.json file is empty.")
-            return
-        for directory in repos:
-            for url in repos[directory]:
-                repo_dirname = os.path.basename(url)
-                if not os.path.exists(repo_dirname):
-                    continue
-                repo = git.Repo(repo_dirname)
-                for remote in repo.remotes:
-                    for remote_url in remote.urls:
-                        if remote_url.endswith('.git'):
-                            remote_url = remote_url[:-4]
-                        if url not in remote_url:
-                            typer.echo(f"Repository {url} in {directory} is not listed in the repoconfig file.")
-        return func(*args, **kwargs)
-    return wrapper
+## STRIPPING
+def rstrip_ssh(link):
+    return link.rstrip("git").rstrip(".")
 
-def add_missing_repositories():
-    repos = read_file_contents(CONFIG_PATH)
-    for directory in directories:
-        if not os.path.exists(directory):
-            continue
-        repo = git.Repo(directory)
-        for remote in repo.remotes:
-            for url in remote.urls:
-                if url.endswith('.git'):
-                    url = url[:-4]
-                if url not in repos.get(directory, []):
-                    typer.echo(f"Adding repository {url} in {directory} to the repoconfig file.")
-                    repos.setdefault(directory, []).append(url)
-    write_data_to_file(CONFIG_PATH, repos)
+def lstrip_ssh(link):
+    return link.lstrip("git").lstrip("@")
 
-@check_repoconfig_file
-def show_repositories():
-    if not os.path.isfile(CONFIG_PATH):
-        typer.echo("The repoconfig.json file does not exist. Please run the 'create' command to generate it.")
-        return
-    repos = read_file_contents(CONFIG_PATH)
-    if not repos:
-        typer.echo("The repoconfig.json file is empty.")
-        add_missing_repositories()
-        typer.echo("Repositories added to repoconfig.json file.")
-        return
-    for directory, urls in repos.items():
-        typer.echo(f"\n{directory}:")
-        for url in urls:
-            typer.echo(f" - {url}")
+def strip_directory(link):
+    return link.split(":")[1].split("/")
 
-def push_changes():
-    repo = git.Repo(os.getcwd())
-    if repo.is_dirty(untracked_files=True):
-        repo.git.add(".")
-        repo.index.commit("Automatic commit")
-        origin = repo.remote(name="origin")
-        origin.push()
-        typer.echo("Changes pushed to remote repository.")
-    else:
-        typer.echo("No changes to push.")
+def strip_platform(link):
+    return lstrip_ssh(link.split(":")[0])
 
-def export_config(file_path: str):
-    data = {}
-    for directory in directories:
-        data[directory] = []
-        for root, dirs, files in os.walk(directory):
-            for file in files:
-                if file == 'config':
-                    path = os.path.join(root, file)
-                    with open(path, 'r') as f:
-                        content = f.read()
-                        match = re.search(r'git@[^@]*\.com:[^:]+/[^/]+', content)
-                        if match:
-                            url = match.group(0)
-                            url = re.sub(r'\s+fetch\s*=.*', '', url)
-                            data[directory].append(url)
-    write_data_to_file(file_path, data)
+def split_repository_directory(link):
+    # .rstrip(".git") strips to much characters
+    parts = strip_directory(rstrip_ssh(link))
+    repository = parts[-1]
+    directories = parts[:-1]
+    return repository, directories
 
-@check_repoconfig_file
-def import_config(file_path: str):
-    data = read_file_contents(file_path)
-    for directory, urls in data.items():
-        for url in urls:
-            clone_repository(directory, url)
-    typer.echo("Repositories imported successfully.")
+def get_alias(platform, repoconfig=REPO_CONFIG_FILE):
+    return read_file_contents(repoconfig)["platforms"][platform]["alias"]
 
-def list_all_repositories(directory_path):
-    repositories = []
-    for root, dirs, files in os.walk(directory_path):
-        for file in files:
-            if file == 'config':
-                path = os.path.join(root, file)
-                with open(path, 'r') as f:
-                    content = f.read()
-                    match = re.search(r'git@[^@]*\.com:[^:]+/[^/]+', content)
-                    if match:
-                        url = match.group(0)
-                        url = re.sub(r'\s+fetch\s*=.*', '', url)
-                        repositories.append(url)
-    return repositories
+## SETUP
+def repoconfig_setup(repoconfig=REPO_CONFIG_FILE):
+    repoconfig_file = read_file_contents(repoconfig)
+    if repoconfig_file["links"] == []:
+        update_repoconfig(SEARCH_PATH)
+        sort_repositories()
+        set_aliases()
+    elif repoconfig_file.get("platforms") == None:
+        sort_repositories()
+        set_aliases()
+    elif repoconfig_file["platforms"] == []:
+        set_aliases()
+        for link in repoconfig["platforms"].keys():
+            if not get_alias(strip_platform(link), repoconfig):
+                set_aliases()
 
-def create_ansible_playbook():
-    PLAYBOOK_PATH = 'playbook.yml'
-    config = read_file_contents(CONFIG_PATH)
-    repo_dict = {}
-    for directory, repos in config.items():
-        for repo_url in repos:
-            repo_name = repo_url.split('/')[-1].split('.')[0]
-            repo_dict[repo_name] = {
-                'dest': os.path.join(directory, repo_name),
-                'repo': repo_url
-            }
-    with open(PLAYBOOK_PATH, 'w') as f:
-        home = "home"
-        f.write('---\n')
-        f.write('- hosts: all\n')
-        f.write('  vars:\n')
-        f.write('    home: ""\n')
-        f.write('  tasks:\n')
-        for repo_name, repo_info in repo_dict.items():
-            f.write(f'  - name: Clone {repo_name}\n')
-            f.write(f'    ansible.builtin.git:\n')
-            f.write(f'      repo: {repo_info["repo"]}\n')
-            f.write(f'      dest: "{{{{{ home }}}}}{repo_info["dest"]}"\n')
-            f.write(f'    ignore_errors: yes\n\n')
-    typer.echo(f'Ansiblel playbook file created at {PLAYBOOK_PATH}.')
+def clone_repos(repoconfig=REPO_CONFIG_FILE):
+    init_repoconfig(SEARCH_PATH)
+    repoconfig_file = read_file_contents(repoconfig)
+    for link in repoconfig_file["links"]:
+        current_path = create_path(get_alias(strip_platform(link)))
+        repository, directories = split_repository_directory(link)
+        for subdir in directories:
+            current_path = os.path.join(current_path, subdir)
+            os.makedirs(current_path, exist_ok=True)
+        repo_path = os.path.join(current_path, repository)
+        Repo.clone_from(link, repo_path)
+        print(f"Repository '{repository}' cloned successfully.")
+    print("Folders and repositories created successfully.")
+
+#clone_repos("repoconfig.json")
+def get_repository_links(directory):
+    remote_urls = []
+    for repo_path in glob.iglob(os.path.join(directory, '**', '.git'), recursive=True):
+        repo = Repo(os.path.dirname(repo_path))
+        remote_urls.extend([remote.url for remote in repo.remotes])
+    return get_unique_links(remote_urls)
+
+def get_unique_links(source, target=[]):
+    for url in source:
+        if url not in target:
+            target.append(url)
+    return target
+
+def sort_repositories(repoconfig=REPO_CONFIG_FILE):
+    repoconfig_file = read_file_contents(repoconfig)
+    repoconfig_file["platforms"] = {}
+    repoconfig_platforms = list(set([strip_platform(link) for link in repoconfig_file["links"]]))
+    for platform in repoconfig_platforms:
+        repoconfig_file["platforms"][platform] = {"alias": "", "repos": []}
+        for link in repoconfig_file["links"]:
+            if platform == strip_platform(link):
+                repoconfig_file["platforms"][platform]["repos"].append(link)
+    write_data_to_file(repoconfig, repoconfig_file)
+
+def set_aliases(repoconfig=REPO_CONFIG_FILE):
+    repoconfig_file = read_file_contents(repoconfig)
+    for platform in repoconfig_file["platforms"]:
+        answer = prompt(f'Set alias for {platform}: ')
+        repoconfig_file["platforms"][platform]["alias"] = answer
+        print(f'Alias for {platform} set to: {repoconfig_file["platforms"][platform]["alias"]}')
+    write_data_to_file(repoconfig, repoconfig_file)
+
+#sort_repositories()
+#set_aliases()
+#update_repo_config("/home/theintegrative/", "test2.json")
+clone_repos()
